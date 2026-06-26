2cda3d798256047b42d56b733280f0c8f30e61b4
oxod <git@oxod.nl>
2026-06-26 22:45:27 +0200

    feat: add scroll indicator with bounce animation to hero

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -1,3 +1,5 @@
+import { ArrowDown } from "lucide-react"
+
 export const HomeSection = () => {
   return <section id="home" className="relative min-h-screen flex flex-col items-center justify-center px-4">
     <div className="container max-w-4xl mx-auto text-center z-10">
@@ -17,6 +19,11 @@ export const HomeSection = () => {
           </a>
         </div>
       </div>
+    </div>
+
+    <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
+      <span>Scroll</span>
+      <ArrowDown className="h-5 w-5 text-primary"/>
     </div>
   </section>
 }
