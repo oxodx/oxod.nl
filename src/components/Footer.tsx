6f719256137ed529c45232e92afcc4ecd1fd494b
oxod <git@oxod.nl>
2026-06-27 01:09:56 +0200

    fix: correct Footer license text to MIT

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import { ArrowUp } from "lucide-react"
 export const Footer = () => {
   return <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
     <p className="text-sm text-muted-foreground">
-      &copy; {new Date().getFullYear()} oxod.nl, All rights reserved.
+      &copy; {new Date().getFullYear()} oxod.nl, MIT.
     </p>
 
     <a href="#home" className="p-2 rounded-full bg-primary/10 hover:bg-primary/20 texxt-primary transition-colors">
