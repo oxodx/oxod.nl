69711b7a865780726c69236c207f8b52665bbe68
oxod <git@oxod.nl>
2026-06-26 20:30:15 +0200

    feat: add section placeholders to Home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,13 @@
 export const Home = () => {
-  return <div>HomePage</div>
+  return <div>
+    {/* Theme Toggle */}
+
+    {/* Background Effects */}
+
+    {/* Navbar */}
+
+    {/* Main Content */}
+  
+    {/* Footer */}
+  </div>
 }
