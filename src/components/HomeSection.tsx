8f3d4dd0935120d9bd1d1697438fa4a8213f9da9
oxod <git@oxod.nl>
2026-06-26 22:41:38 +0200

    feat: add animation and styling to HomeSection subtitle

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -7,7 +7,7 @@ export const HomeSection = () => {
           <span className="text-primary opacity-0 animate-fade-in-delay-1"> oxod</span>
         </h1>
 
-        <p>
+        <p className="text-lg md:text-xl text-muted-foreground max-2-2xl mx-auto opacity-0 animate-fade-in-delay-4">
           I create random stuff
         </p>
       </div>
