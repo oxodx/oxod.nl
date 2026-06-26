3199778f0b21c9508f2e9f8de39644bca3a40bb0
oxod <git@oxod.nl>
2026-06-26 22:46:01 +0200

    style: add styling to scroll indicator label

--- src/components/HomeSection.tsx
+++ src/components/HomeSection.tsx
@@ -22,7 +22,7 @@ export const HomeSection = () => {
     </div>
 
     <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 flex flex-col items-center animate-bounce">
-      <span>Scroll</span>
+      <span className="text-sm text-muted-foreground mb-2">Scroll</span>
       <ArrowDown className="h-5 w-5 text-primary"/>
     </div>
   </section>
